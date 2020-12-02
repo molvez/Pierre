3cd358e9e0d0fec18ddd1e2363d8a8df31596ee9
molvez <pierre.molvez@outlook.fr>
2020-12-02 16:01:48 +0100

    fait

--- test222222.php
+++ test222222.php
@@ -1,7 +1,7 @@
-fdsfsdfsdf
-sdf
-sd
-<fieldset>sdf
-    sd
-    
-</fieldset>
+echo "# Test1" >> LISEZMOI.md
+git init
+git add README.md
+git commit -m "premier commit"
+git branch -M main
+git remote ajouter l'origine https://github.com/molvez/pierre.git
+git push -u origin main
