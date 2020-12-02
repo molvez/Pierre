13e6a9d2407b54c08f973bdb1904cf2d4ca96415
molvez <pierre.molvez@outlook.fr>
2020-12-02 16:05:55 +0100

    update

--- TEST2.php
+++
@@ -1,7 +0,0 @@
-echo "# Test1" >> TEST2.php
-git init
-git add README.md
-git commit -m "premier commit"
-git branch -M main
-git remote ajouter l'origine https://github.com/molvez/pierre.git
-git push -u origin main
